182b9669f97a0bf3eead83a857878a3a954f3128
Reagan Fwamba <youknowreaganfwambaa@gmail.com>
2026-07-27 06:19:53 +0300

    Started Class discussion and tackled all the way to Java Class Methods

---
+++ notes/phase-1-foundations/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    public String name = "John"; // default value
+    public static void main(String[] args) {
+    Person myObj = new Person();
+    System.out.println(myObj.name); // Output: John
+    
+}
+}
